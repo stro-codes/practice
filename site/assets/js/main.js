c95b13ed6f77c52b4b82e98a9c0a383e13fd7268
Strother Woog <strowoog@gmail.com>
2019-07-15 17:58:10 -0700

    add additional posts, will make more efficient through json data structured plog post database

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -31,6 +31,26 @@ function closeCardAlert() {
     document.getElementById('cardAlert').style.visibility = 'hidden';
 }
 
+function loadHome() {
+    document.getElementById('content').style.display = 'none';
+    document.getElementById('content').innerHTML = '';
+    document.getElementById('main_page').style.display = 'initial';
+}
+
+function loadAbout() {
+    document.getElementById('main_page').style.display = 'none';
+    document.getElementById('content').innerHTML = '<object type="text/html" data="about.html"></object>';
+    document.getElementById('content').style.display = 'initial';
+}
+
+function addBlogPost() {
+    document.getElementById('main_page').style.display = 'none';
+    document.getElementById('content').innerHTML = '<object type="text/html" style="width:100%; height:100%" data="newBlogPost.html"></object>';
+    document.getElementById('content').style.display = 'initial';
+    document.getElementById('post_container').innerHTML = document.getElementById('post_container').innerHTML + ('<div class="w3-card-4 w3-margin w3-white"> <div class="w3-container w3-blue"><h2 class="post_header">Todays Header</h2> <h4 class="post_date">07/15/19</h4></div><form class="w3-container w3-padding w3-margin"><p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p></form></div>');
+
+}
+
 var testObject = {
     name: "testy",
     age: 1,
