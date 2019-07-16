2b00327c1e8510274e02f612a3dca9e6706a8543
Strother Woog <strowoog@gmail.com>
2019-07-15 18:51:47 -0700

    fixed images link in nav bar

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -54,6 +54,7 @@ function addBlogPost() {
 function loadImages() {
     document.getElementById('main_page').style.display = 'none';
     document.getElementById('content').innerHTML = '<object type="text/html" data="images.html" style="width:100%;height:100%"></object>';
+    document.getElementById('content').style.display = 'initial';
 } 
 
 var testObject = {
