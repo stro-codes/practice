691a51737ac016dca4e0cf3407ddd0b0e0bfd725
Strother Woog <strowoog@gmail.com>
2019-07-18 18:58:18 -0700

    started learning bootstrap framework
    after using w3 schools css stylesheet, I decided to use this more popular and dynamic framework

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,32 +1,3 @@
-function myfuncor() {
-    var x = document.getElementById('first').innerHTML;
-    var y = document.getElementById('last').innerHTML;
-    return result = "hello world!";
-}
-
-function findAndSwap() {
-    // grab attributes for a node to be found
-    var tag = document.getElementById('first').value;
-    var classs = document.getElementById('last').value;
-    var id = document.getElementById('id').value;
-
-    // grab data from node that is found
-    var x = document.getElementsByClassName('classs').getElementsByTagName('tag').getElementById('id').innerHTML;
-
-    // display that text
-    document.getElementById('test').value = x;
-}
-
-function fudge() {
-    var tag = document.getElementById('first').value;
-    var classs = document.getElementById('last').value;
-
-    document.getElementById('a').innerHTML = tag;
-    document.getElementById('b').innerHTML = classs;
-
-    document.getElementById('cardAlert').style.visibility = 'visible';
-}
-
 function closeCardAlert() {
     document.getElementById('cardAlert').style.visibility = 'hidden';
 }
@@ -45,21 +16,26 @@ function loadAbout() {
 
 function addBlogPost() {
     document.getElementById('main_page').style.display = 'none';
-    document.getElementById('content').innerHTML = '<object type="text/html" style="width:100%; height:100%" data="newBlogPost.html"></object>';
+    document.getElementById('content').innerHTML = '<iframe src="newBlogPost.html" style="width:100%; height:600px"></iframe>';
     document.getElementById('content').style.display = 'initial';
-    document.getElementById('post_container').innerHTML = document.getElementById('post_container').innerHTML + ('<div class="w3-card-4 w3-margin w3-white"> <div class="w3-container w3-blue"><h2 class="post_header">Todays Header</h2> <h4 class="post_date">07/15/19</h4></div><form class="w3-container w3-padding w3-margin"><p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p></form></div>');
+}
+
+function insertNewPost() {
 
 }
 
 function loadImages() {
     document.getElementById('main_page').style.display = 'none';
-    document.getElementById('content').innerHTML = '<object type="text/html" data="images.html" style="width:100%;height:auto"></object>';
+    document.getElementById('content').innerHTML = '<object type="text/html" data="images.html" style="width:100%;height:800px"></object>';
     document.getElementById('content').style.display = 'initial';
 } 
 
-var testObject = {
-    name: "testy",
-    age: 1,
-    grades: [],
-};
+var myPost = [
+    {
+        "Title": "Test Post",
+        "Date": "07/15/19",
+        "Body": "Testing."
+    }
+]
+
 
