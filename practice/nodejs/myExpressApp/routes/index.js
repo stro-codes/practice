701766aaabca818b9f7b7c7c713e24aba8ef5136
Strother Woog <strowoog@gmail.com>
2019-07-31 13:01:46 -0700

    working on routing/RESTful API practice
    added a little more bootstrap just to see how the notation differs from html to ejs html notation

--- practice/nodejs/myExpressApp/routes/index.js
+++ practice/nodejs/myExpressApp/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'the test site' });
+  console.log("get home page");
+});
+
+/* GET features page. */
+router.get('/features', function(req, res, next) {
+  //res.send("features");
+  res.render('features', { title: 'the test site' });
+  console.log("get features page");
 });
 
 module.exports = router;
